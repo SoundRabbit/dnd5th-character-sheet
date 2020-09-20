156771cd30cd80f853e2237402ec122d018bd827
SoundRabbit <soundrabbitstd@gmail.com>
2020-09-20 22:00:32 +0900

    feat: 成長履歴追加ボタンを作成

--- src/component/experience.rs
+++ src/component/experience.rs
@@ -32,31 +32,50 @@ fn render(_: &State, _: Vec<Html>) -> Html {
     Html::div(
         Attributes::new().class("pure-form").class("experience"),
         Events::new(),
-        vec![Html::component(
-            key_value::new().with(key_value::Props {}),
-            vec![
-                Html::text("初期クラス"),
-                Html::component(
-                    select::new().with(select::Props {
-                        suid: crate::suid!(),
-                        option: vec![
-                            String::from("ウィザード"),
-                            String::from("ウォーロック"),
-                            String::from("クレリック"),
-                            String::from("ソーサラー"),
-                            String::from("ドルイド"),
-                            String::from("バード"),
-                            String::from("バーバリアン"),
-                            String::from("パラディン"),
-                            String::from("ファイター"),
-                            String::from("モンク"),
-                            String::from("レンジャー"),
-                            String::from("ローグ"),
-                        ],
-                    }),
-                    vec![],
-                ),
-            ],
-        )],
+        vec![
+            Html::h2(
+                Attributes::new(),
+                Events::new(),
+                vec![Html::text("習得済みクラス")],
+            ),
+            Html::h2(
+                Attributes::new(),
+                Events::new(),
+                vec![Html::text("経験点の獲得/消費")],
+            ),
+            Html::component(
+                key_value::new().with(key_value::Props {}),
+                vec![
+                    Html::text("初期クラス"),
+                    Html::component(
+                        select::new().with(select::Props {
+                            suid: crate::suid!(),
+                            option: vec![
+                                String::from("ウィザード"),
+                                String::from("ウォーロック"),
+                                String::from("クレリック"),
+                                String::from("ソーサラー"),
+                                String::from("ドルイド"),
+                                String::from("バード"),
+                                String::from("バーバリアン"),
+                                String::from("パラディン"),
+                                String::from("ファイター"),
+                                String::from("モンク"),
+                                String::from("レンジャー"),
+                                String::from("ローグ"),
+                            ],
+                        }),
+                        vec![],
+                    ),
+                ],
+            ),
+            Html::button(
+                Attributes::new()
+                    .class("pure-button")
+                    .class("pure-button-primary"),
+                Events::new(),
+                vec![Html::text("追加")],
+            ),
+        ],
     )
 }
